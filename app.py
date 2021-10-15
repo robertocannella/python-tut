09de0ed8b1f7f2dca1cc8ad5df3fc8ce2b2ba761
robertocannella <robertocannella@gmail.com>
2021-10-14 22:01:25 -0400

    types and type checking

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-students_count = 1000   # integer
-rating = 4.99           # float
-is_published = False    # boolean
-course_name = """       
-This is a string that 
-has multiple lines
-"""                     # multiline string
+students_count = 1000   # Python is dynamically typed
+print(type(students_count))
+print(type("HELLO"))
+print(type(1.1))
+print(type(False))
 
-# other variable declarations
-x = 1
-y = 2
-x, y = 1, 2
-x = y = 1
+age: int = 20   # installing mypy linter allows for type checking
+# compiler error is shown though will not throw an error
+age = "Python"
