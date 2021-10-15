689088d644cb410680ce0cacf210e2679262832b
robertocannella <robertocannella@gmail.com>
2021-10-14 21:48:57 -0400

    variables

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-22 + 3
-unit_price = 2
-print(unit_price)
+students_count = 1000   # integer
+rating = 4.99           # float
+is_published = False    # boolean
+course_name = """       
+This is a string that 
+has multiple lines
+"""                     # multiline string
+
+# other variable declarations
+x = 1
+y = 2
+x, y = 1, 2
+x = y = 1
