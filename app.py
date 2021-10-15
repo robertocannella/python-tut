981938a47df9279ef57fc9352a124a38f794b38d
robertocannella <robertocannella@gmail.com>
2021-10-14 23:32:40 -0400

    strings

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
-# immutable types (new memory allocation when changing value)
-x: int = 1
+from types import coroutine
 
-print(id(x))
 
-x = x + 1
-print(id(x))
+string = "This is A String"
 
-# mutable types  (smae memery address)
+# String indexing
+print(len(string))
+print(string[0])
+print(string[-1])
 
-y: list = [1, 2, 3]
-print(id(y))
-y.append(4)
-print(id(y))
+# Slicing strings
+print(string[0:3])
+print(string[:3])  # excluding a start index, python will imply 0index
+
+# Escape sequences (inherited from C language)
+# \'  \"  \\ \n
+msg: str = "Python \"Programming\""
+print(msg)
+
+# Concatination
+first: str = "Roberto"
+last: str = "Cannella"
+full: str = first + " " + last
+print(full)
+
+# String Formatting {expressions can be handled inside curly braces}
+alternative_full: str = f"{first} {last}"
+print(alternative_full)
+
+course: str = "Python Programming"
+print(course.upper())  # UPPERCASE
+print(course.lower())  # lowercase
+print(course.title())  # Title Case
+print(course.count("P"))  # counts occurrances
+print(course.strip())  # remove white space
+print(course.find("Pro"))  # search (returns -1 if false)
+print(course.replace("P", "-"))  # replace
+print("Programming" in course)  # returns boolean
+print("Programming" not in course)  # returns boolean
