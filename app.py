a2ddecd5a33da6b1ee1bfc540090a240762155cf
robertocannella <robertocannella@gmail.com>
2021-10-15 22:04:40 -0400

    import math

--- app.py
+++ app.py
@@ -1,36 +1,5 @@
-x = 10  # decimal
-x = 0b10  # binary
-print(x)  # binary value
-print(bin(x))  # binary representation
+import math
 
-x = 0x12c  # hexidecimal
-print(x)  # decimal value
-print(hex(x))  # hexidecimal representation
-
-# complex numbers
-
-y: complex = (1 + 2j)
-print(y)
-
-# arithmetic
-x = 10 + 3  # add
-print(x)
-x = 10 - 3  # subtract
-print(x)
-x = 10 * 3  # multiply
-print(x)
-m = 10 / 3  # divide (no remainder)
-print(m)
-x = 10 // 3  # divide (with remainder)
-print(x)
-x = 10 % 2  # return remaider
-print(x)
-x = 10 ** 3  # raise to power
-print(x)
-
-# augmented assignment operators for all arithmetic operations
-
-x += 1
-print(x)
-
-# NO fuctions for increment or decrement x-- x++
+PI = 3.14
+print(round(PI))
+print(math.floor(PI))
